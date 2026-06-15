12128d9cc034c43da47aa07d3f9843735288abc4
LUEMDEV <chaluemponb@gmail.com>
2026-06-15 20:29:26 +0700

    Remove duplicate folder and cleanup structure

--- SutHackathon-main/App.js
+++
@@ -1,7 +0,0 @@
-// App.js
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
-
-export default function App() {
-  return <AppNavigator />;
-}
--- SutHackathon-main/components/test1.js
+++
@@ -1,3 +0,0 @@
-import Test1Screen from "../screens/Test1Screen";
-
-Test1Screen
